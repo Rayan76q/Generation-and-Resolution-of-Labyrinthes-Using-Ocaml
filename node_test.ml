1730b2f0c84dfc13a02b4af86a5b9b652da76001
antoine.el-kassis <antoine.el-kassis@universite-paris-saclay.fr>
2024-01-17 00:21:29 +0100

    finishing test files for laby grille and node. Focused on node and tested every function in Node module. Tested main to see if there is any error or mismatch but of course i can't add tests because it is interactive

--- node_test.ml
+++ node_test.ml
@@ -10,10 +10,6 @@ let m = Node.cree_noeud (1,2) [(Node.cree_noeud (2,2) []); (Node.cree_noeud (1,3
 let () = Node.print_noeud m
 
 
-(*
-let m = Node.supprime_connexion m (1,2)    plante
-let () = Node.print_noeud m
-*)
 
 (*
 Erreur, supprime_connexion
@@ -36,3 +32,40 @@ let (n , m ) = Node.ajoute_connexion n m
 
 let () = Node.print_noeud n
 let () = Node.print_noeud m
+
+let () =
+  let(n, m)=Node.supprime_connexion n m in
+  Node.print_noeud n; Node.print_noeud m
+;;
+
+
+let ()=Printf.printf "--------------------------------\n"
+
+let()= 
+  let n= Node.set_visite n true in
+  let m=Node.set_visite m true in
+  Node.print_noeud n; Node.print_noeud m;
+;;
+
+let()= 
+  let n= Node.set_visite n false in
+  let mm=Node.set_visite m true in
+  Node.print_noeud n; Node.print_noeud mm
+;;
+
+let()=
+  let fon_fail_node x y w z=
+    let m = Node.cree_noeud (x,y) [] in
+    let n = Node.cree_noeud (w,z) [] in
+    try 
+      let (n , m ) = Node.ajoute_connexion n m in
+      Node.print_noeud n; Node.print_noeud m;
+    with
+    | Failure msg -> Printf.printf "fail: %s\n" msg; Printf.printf "-----------------------\n"
+  in
+  fon_fail_node 1 1 2 2;
+  fon_fail_node 1 1 2 1;
+  fon_fail_node 2 2 3 3;
+  fon_fail_node 1 0 0 1;
+  fon_fail_node 1 0 1 0 (*fail si meme node evidemment*)
+;;
